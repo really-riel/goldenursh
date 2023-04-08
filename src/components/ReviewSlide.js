2ff4e73cbd70145fac45976cdd6f3a4d1effb2df
really-riel <110791013+really-riel@users.noreply.github.com>
2023-04-08 09:14:02 +0100

    third commit

--- src/components/ReviewSlide.js
+++ src/components/ReviewSlide.js
@@ -1,14 +1,44 @@
 import React from "react";
+import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import "swiper/css/free-mode";
+import { FreeMode, Navigation } from "swiper";
+
 import ReviewCard from "./ReviewCard";
+import { useState } from "react";
+import { useRef } from "react";
 
 const ReviewSlide = ({ customerReview }) => {
+  const [init, setInit] = useState();
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
   return (
     <div className="reviewSlide">
-      {customerReview?.map((data, index) => (
-        <ReviewCard data={data} key={index} />
-      ))}
+      <Swiper
+        spaceBetween={5}
+        slidesPerView={3}
+        freeMode={true}
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+        modules={[FreeMode, Navigation]}
+        onInit={() => setInit(true)}
+      >
+        {customerReview?.map((data, index) => (
+          <SwiperSlide key={index} className="reviewCardContainer">
+            <ReviewCard data={data} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <div className="sliderBtnLeft" ref={prevRef}>
+        <IoIosArrowDropleft />
+      </div>
+      <div className="sliderBtnRight" ref={nextRef}>
+        <IoIosArrowDropright />
+      </div>
     </div>
   );
 };
