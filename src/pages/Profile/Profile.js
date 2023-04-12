3b103cf5e0e2ecf21f0188d843067573a3a45de4
really-riel <110791013+really-riel@users.noreply.github.com>
2023-04-12 18:16:47 +0100

    working on the profile tablet and laptop version

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -7,48 +7,65 @@ const Profile = () => {
   return (
     <main className="Profile">
       <section className="profileDetails">
-        <motion.div
-          initial={{ opacity: 0, x: -300 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: 300 }}
-          transition={{ delay: 0.2, duration: 1 }}
-          className="container"
-        >
-          <div className="imageContainer">
-            <h2 className="mobileHeading">Profile Details</h2>
-            <img src={user.image} alt="profile pic" />
-          </div>
-          <div className="formContainer">
-            <form>
-              <label htmlFor="profileName">Name</label>
-              <input type="text" id="profileName" value={user.name} readOnly />
-
-              <label htmlFor="profileFirstName">E-mail</label>
-              <input
-                type="email"
-                id="profileFirstName"
-                value={user.email}
-                readOnly
-              />
-
-              <label htmlFor="phone">Phone</label>
-              <input type="tel" id="phone" value={user.phone} readOnly />
-
-              <label htmlFor="address">Address</label>
-              <input
-                type="text"
-                id="address"
-                readOnly
-                value={user.address.length > 0 ? user.address : "ADD ADDRESS"}
-              />
-            </form>
+        <div className="tabletSection1 ">
+          <div className="tabletHeading">
+            <h2>Profile Details</h2>
             <Link to={"/profile/edit"}>
-              <p role="button" className="editBtn">
-                Edit Profile
-              </p>
+              <h2 role="button" className="tabletEditBtn">
+                Edit
+              </h2>
             </Link>
           </div>
-        </motion.div>
+
+          <motion.div
+            initial={{ opacity: 0, x: -300 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 300 }}
+            transition={{ delay: 0.2, duration: 1 }}
+            className="container"
+          >
+            <div className="imageContainer">
+              <h2 className="mobileHeading">Profile Details</h2>
+              <img src={user.image} alt="profile pic" />
+            </div>
+            <div className="formContainer">
+              <form>
+                <label htmlFor="profileName">Name</label>
+                <input
+                  type="text"
+                  id="profileName"
+                  value={user.name}
+                  readOnly
+                />
+
+                <label htmlFor="profileFirstName">E-mail</label>
+                <input
+                  type="email"
+                  id="profileFirstName"
+                  value={user.email}
+                  readOnly
+                />
+
+                <label htmlFor="phone">Phone</label>
+                <input type="tel" id="phone" value={user.phone} readOnly />
+
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  id="address"
+                  readOnly
+                  value={user.address.length > 0 ? user.address : "ADD ADDRESS"}
+                />
+              </form>
+              <Link to={"/profile/edit"}>
+                <p role="button" className="editBtn">
+                  Edit Profile
+                </p>
+              </Link>
+            </div>
+          </motion.div>
+        </div>
+        <div className="tabletSection2 shapedividers_com-1822"></div>
       </section>
     </main>
   );
--- src/pages/Profile/ProfileEdit.js
+++ src/pages/Profile/ProfileEdit.js
@@ -12,68 +12,71 @@ const ProfileEdit = () => {
   return (
     <main className="Profile ProfileEdit">
       <section className="profileDetails">
-        <motion.div
-          initial={{ opacity: 0, x: 300 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: -300 }}
-          transition={{ delay: 0.2, duration: 1 }}
-          className="container"
-        >
-          <div className="imageContainer">
-            <h2 className="mobileHeading">Edit Profile</h2>
-            <label htmlFor="editImage">
-              <figure>
-                <img src={editImage} alt="profile pic" />
-                <div className="editPhoto">
-                  Edit Photo
-                  <AiOutlineEdit />
-                </div>
-              </figure>
-            </label>
-          </div>
-          <div className="formContainer">
-            <form>
-              <input
-                type="file"
-                accept="image/*"
-                id="editImage"
-                className="hidden"
-              />
-              <label htmlFor="profileName">Name</label>
-              <input
-                type="text"
-                id="profileName"
-                value={editName}
-                onChange={(e) => setEditName(e.target.value)}
-              />
+        <div className="tabletSection1">
+          <motion.div
+            initial={{ opacity: 0, x: 300 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -300 }}
+            transition={{ delay: 0.2, duration: 1 }}
+            className="container"
+          >
+            <div className="imageContainer">
+              <h2 className="mobileHeading">Edit Profile</h2>
+              <label htmlFor="editImage">
+                <figure>
+                  <img src={editImage} alt="profile pic" />
+                  <div className="editPhoto">
+                    Edit Photo
+                    <AiOutlineEdit />
+                  </div>
+                </figure>
+              </label>
+            </div>
+            <div className="formContainer">
+              <form>
+                <input
+                  type="file"
+                  accept="image/*"
+                  id="editImage"
+                  className="hidden"
+                />
+                <label htmlFor="profileName">Name</label>
+                <input
+                  type="text"
+                  id="profileName"
+                  value={editName}
+                  onChange={(e) => setEditName(e.target.value)}
+                />
 
-              <label htmlFor="profileFirstName">E-mail</label>
-              <input
-                type="email"
-                id="profileFirstName"
-                value={editEmail}
-                onChange={(e) => setEditEmail(e.target.value)}
-              />
+                <label htmlFor="profileFirstName">E-mail</label>
+                <input
+                  type="email"
+                  id="profileFirstName"
+                  value={editEmail}
+                  onChange={(e) => setEditEmail(e.target.value)}
+                />
 
-              <label htmlFor="phone">Phone</label>
-              <input
-                type="tel"
-                id="phone"
-                value={editPhone}
-                onChange={(e) => setEditPhone(e.target.value)}
-              />
+                <label htmlFor="phone">Phone</label>
+                <input
+                  type="tel"
+                  id="phone"
+                  value={editPhone}
+                  onChange={(e) => setEditPhone(e.target.value)}
+                />
 
-              <label htmlFor="address">Address</label>
-              <input
-                type="text"
-                id="address"
-                value={editAddress}
-                onChange={(e) => setEditAddress(e.target.value)}
-              />
-              <button>Complete</button>
-            </form>
-          </div>
-        </motion.div>
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  id="address"
+                  value={editAddress}
+                  onChange={(e) => setEditAddress(e.target.value)}
+                />
+                <button>Complete</button>
+              </form>
+            </div>
+          </motion.div>
+        </div>
+        <div className="tabletSection2 shapedividers_com-1822"></div>
       </section>
     </main>
   );
