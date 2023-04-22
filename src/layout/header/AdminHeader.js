e2582385c038a0c385625e1b855e294ac742d352
really-riel <110791013+really-riel@users.noreply.github.com>
2023-04-22 11:14:52 +0100

    Update AdminHeader.js

--- src/layout/header/AdminHeader.js
+++ src/layout/header/AdminHeader.js
@@ -8,27 +8,30 @@ import SideNav from "../nav/SideNav";
 import TabletNav from "../nav/TabletNav";
 import { Link } from "react-router-dom";
 import AdminSideNav from "../nav/AdminSideNav";
+import { RequireAdminLink } from "../../components/RequireLinks";
 
 const AdminHeader = () => {
   const [isSideNavOpen, setIsSideNavOpen] = useState(false);
   const { width } = useWindowSize();
   return (
-    <header className="adminHeader">
-      <motion.div className="hamburgerMenu" whileTap={{ scale: 0.8 }}>
-        <GiHamburgerMenu
-          size={width >= 768 ? "2.5rem" : "1.5rem"}
-          onClick={() => setIsSideNavOpen(!isSideNavOpen)}
-        />
-      </motion.div>
-      <div className="logoContainer">
-        {width < 1024 && <TabletNav />}
-        <Link to={"/"}>
-          <Logo />
-        </Link>
-      </div>
+    <RequireAdminLink>
+      <header className="adminHeader">
+        <motion.div className="hamburgerMenu" whileTap={{ scale: 0.8 }}>
+          <GiHamburgerMenu
+            size={width >= 768 ? "2.5rem" : "1.5rem"}
+            onClick={() => setIsSideNavOpen(!isSideNavOpen)}
+          />
+        </motion.div>
+        <div className="logoContainer">
+          {width < 1024 && <TabletNav />}
+          <Link to={"/"}>
+            <Logo />
+          </Link>
+        </div>
 
-      {isSideNavOpen && <AdminSideNav setIsSideNavOpen={setIsSideNavOpen} />}
-    </header>
+        {isSideNavOpen && <AdminSideNav setIsSideNavOpen={setIsSideNavOpen} />}
+      </header>
+    </RequireAdminLink>
   );
 };
 
