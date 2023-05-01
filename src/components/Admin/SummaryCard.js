00a4de4368c2100604225398fb82c99d9d870e50
really-riel <110791013+really-riel@users.noreply.github.com>
2023-05-01 14:27:14 +0100

    working on admin dashboard

--- src/components/Admin/SummaryCard.js
+++ src/components/Admin/SummaryCard.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
-import { FaMoneyBill } from "react-icons/fa";
+import { TbPackages } from "react-icons/tb";
+import { FcMoneyTransfer } from "react-icons/fc";
 import { HiChartBar } from "react-icons/hi";
+import { IoIosPeople } from "react-icons/io";
 
 const SummaryCard = ({ total, category, type }) => {
   const [icon, setIcon] = useState("");
@@ -9,15 +11,45 @@ const SummaryCard = ({ total, category, type }) => {
 
   console.log(category.split(" ")[1]);
 
-  /* switch (category.split(" ")[1]) {
-    case "Sales":
-      setIcon("HiChartBar");
-  } */
+  useEffect(() => {
+    switch (category.split(" ")[1]) {
+      case "Sales":
+        setIconBgColor("orange");
+        break;
+      case "Revenue":
+        setIconBgColor("skyblue");
+        break;
+      case "Orders":
+        setIconBgColor("yellowgreen");
+        break;
+      case "Users":
+        setIconBgColor("purple");
+        break;
+      default:
+        break;
+    }
+  }, [category]);
 
   return (
     <div className="summaryCard">
       <div className="summaryIconContainer">
-        <div className="summaryIcon"></div>
+        <div className="summaryIcon" style={{ backgroundColor: iconBgColor }}>
+          {(() => {
+            switch (category.split(" ")[1]) {
+              case "Sales":
+                return <HiChartBar />;
+              case "Revenue":
+                return <FcMoneyTransfer />;
+              case "Orders":
+                return <TbPackages />;
+              case "Users":
+                return <IoIosPeople />;
+
+              default:
+                return null;
+            }
+          })()}
+        </div>
       </div>
       <div className="summaryDetails">
         <p className="number">{total}</p>
--- src/layout/header/AdminHeader.js
+++ src/layout/header/AdminHeader.js
@@ -9,10 +9,15 @@ import TabletNav from "../nav/TabletNav";
 import { Link } from "react-router-dom";
 import AdminSideNav from "../nav/AdminSideNav";
 import { RequireAdminLink } from "../../components/RequireLinks";
+import { HiOutlineMail } from "react-icons/hi";
+import { useStoreState } from "easy-peasy";
+import { BiBell } from "react-icons/bi";
 
 const AdminHeader = () => {
+  const { user } = useStoreState((state) => state.auth);
   const [isSideNavOpen, setIsSideNavOpen] = useState(false);
   const { width } = useWindowSize();
+
   return (
     <RequireAdminLink>
       <header className="adminHeader">
@@ -27,6 +32,26 @@ const AdminHeader = () => {
           <Link to={"/"}>
             <Logo />
           </Link>
+        </div>
+        <div className="laptopHeaderNav">
+          <ul>
+            <Link to={"/"}>
+              <li>Home</li>
+            </Link>
+            <li className="icon">
+              <HiOutlineMail />
+            </li>
+            <li className="icon">
+              <BiBell />
+            </li>
+            <li className="userDetails">
+              <img src={user.image} alt="" />
+              <p>
+                {user.name} <br />
+                <span>Admin</span>
+              </p>
+            </li>
+          </ul>
         </div>
 
         {isSideNavOpen && <AdminSideNav setIsSideNavOpen={setIsSideNavOpen} />}
